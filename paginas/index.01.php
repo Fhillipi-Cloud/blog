e409934c6a224089d780e56cb441d27f36d1f4ea
Fhillipi-Cloud <128641523+Fhillipi-Cloud@users.noreply.github.com>
2024-04-10 11:34:18 -0300

    upload projeto crud post

---
+++ paginas/index.01.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+for ($num = 0; $num <= 14; $num++) {
+  $nome = ['Allana', 'Ana', 'João', 'Maria', 'Pedro', 'Paulo',            'José', 'Marcos', 'Mariana', 'Júlia', 'Juliane',               'Anita', 'Laura', 'Luiza', 'Larissa'];
+  
+  echo "O nome $nome[$num] é o número ".($num+1)."<br>";
+ }
+*/
+
+$num = 11;
+ do {
+   echo "$num";
+   $num++;
+ } while ($num != 10);
+
+// while ($num <= 10){
+//    echo "$num";
+//    $num++;
+// }
+?> 
