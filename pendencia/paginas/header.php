26709ecf41c3e88b7f395cb86c87e9025f65e2e9
Fhillipi-Cloud <128641523+Fhillipi-Cloud@users.noreply.github.com>
2024-04-10 11:31:47 -0300

    Add files via upload

---
+++ pendencia/paginas/header.php
@@ -0,0 +1,13 @@
+<?php
+  function cabecalho(){
+    echo "
+    <html>
+    <head>
+      <link rel='stylesheet' type='text/css' href='css/estilo.css'>
+    </head>
+    <body>
+      <header>
+        <h1>Arcoplex Cinema</h1>
+      </header>";
+  }
+?>
---
+++ pendencia/paginas/menu.inc.php
@@ -0,0 +1,12 @@
+<?php
+  function menu(){
+    echo "
+      <ul>
+        <li><a>Início</a></li>
+        <li><a>Sobre</a></li>
+        <li><a>Contato</a></li>
+        <li><a>Localização</a></li>
+        <li><a href='paginas/PostList.php'>Admin</a></li>
+      </ul>";
+  }
+?>
